1988098fba87a776dddeec96e3fa2f4d62ea1bde
Oleh Stepaniuk <oleh.stepaniuk@chatbots.studio>
2022-03-18 18:35:49 +0200

    login form style

--- client/src/components/loginForm.tsx
+++ client/src/components/loginForm.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import React, { FC, useContext, useState } from 'react';
 import { Context } from '../index';
+import style from './loginForm.module.css';
 
 const LoginForm: FC = () => {
   const [email, setEmail] = useState<string>('');
@@ -8,21 +9,33 @@ const LoginForm: FC = () => {
   const { store } = useContext(Context);
 
   return (
-    <div>
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        type='text'
-        placeholder='Email'
-      />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        type='text'
-        placeholder='Password'
-      />
-      <button onClick={() => store.login(email, password)}>Login</button>
-      <button onClick={() => store.registration(email, password)}>
-        Sing Up
-      </button>
+    <div className={style.form}>
+      <div className={style.card}>
+        <input 
+          className={style.input}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+          placeholder='Email'
+        />
+        <input 
+          className={style.input}
+          onChange={(e) => setPassword(e.target.value)}
+          type='password'
+          placeholder='Password'
+        />
+        <div className={style.btns}>
+          <button 
+            className={style.btn}
+            onClick={() => store.login(email, password)}>
+            Login
+          </button>
+          <button 
+            className={style.btn}
+            onClick={() => store.registration(email, password)}>
+            Sing Up
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
